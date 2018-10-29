ac9da303d84022ef6e6a2c5978ee67447ecdc579
Pedro Melendez <pmelendezrivera@mail.unicundi.edu.co>
2018-10-29 08:37:09 -0500

    Update PersonaDAO.java

--- proyectoBD/PersonaDAO.java
+++ proyectoBD/PersonaDAO.java
@@ -1,7 +1,10 @@
 package modelo.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import modelo.conexion.Conexion;
 import modelo.vo.PersonaVO;
@@ -36,7 +39,8 @@ public class PersonaDAO {
            }
     }
     
-     	public PersonaVO buscarPersona(int codigo) 
+    
+ 	public PersonaVO buscarPersona(int codigo) 
 	{
             PersonaVO persona= new PersonaVO();
             Conexion conex= new Conexion(); 
@@ -45,12 +49,42 @@ public class PersonaDAO {
         try {
             pst = conex.getConnection().prepareStatement(sql);
             pst.setInt(1, codigo);
-            
+            ResultSet res = pst.executeQuery();
+            while(res.next()){
+                persona.setIdPersona(Integer.parseInt(res.getString("id")));
+                persona.setNombrePersona(res.getString("nombre"));
+                persona.setEdadPersona(Integer.parseInt(res.getString("edad")));
+                persona.setProfesionPersona(res.getString("profesion"));
+                persona.setTelefonoPersona(Integer.parseInt(res.getString("telefono")));
+            }
+             res.close();
+             conex.desconectar();
         } catch (SQLException ex) { 
       		JOptionPane.showMessageDialog(null, "Error, no se conecto");}
 	    			
-	
         return persona;
-        } 
+        }
+        
+        public void modificarPersona(PersonaVO persona){
+            Conexion conex = new Conexion();
+            PreparedStatement pst = null;
+            String sql="UPDATE persona SET id = ?, nombre = ?, edad=?, "
+                    + "profesion=?, telefono=? WHERE id = ? ";
+        try {
+            pst = conex.getConnection().prepareStatement(sql);
+            pst.setInt(1, persona.getIdPersona());
+            pst.setString(2, persona.getNombrePersona());
+            pst.setInt(3, persona.getEdadPersona());
+            pst.setString(4, persona.getProfesionPersona());
+            pst.setInt(5, persona.getTelefonoPersona());
+            pst.setInt(6,persona.getIdPersona() );
+            pst.executeUpdate();
+            JOptionPane.showMessageDialog(null,"Registro actualizado");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error de conecion a la BD");
+         }      
+  }
+        
+    
             
 }
