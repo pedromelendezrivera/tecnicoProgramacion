8de9f7c32e2b1878b7cc7c8ffb2654487236115f
Pedro Melendez <pmelendezrivera@mail.unicundi.edu.co>
2018-10-31 09:43:05 -0500

    Add files via upload

---
+++ proyectoBD/controlador/Principal.java
@@ -0,0 +1,46 @@
+package controlador;
+
+import vista.VentanaBuscar;
+import vista.VentanaPrincipal;
+import vista.VentanaRegistro;
+
+public class Principal {
+	
+	Logica miLogica;
+	VentanaPrincipal miVentanaPrincipal;
+	VentanaBuscar miVentanaBuscar;
+	VentanaRegistro miVentanaRegistro;
+	Coordinador miCoordinador;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+            Principal principal = new Principal();
+	
+	}
+
+    public Principal() {
+                /*Se instancian las clases*/
+		miVentanaPrincipal=new VentanaPrincipal();
+		miVentanaRegistro=new VentanaRegistro();
+		miVentanaBuscar= new VentanaBuscar();
+		miLogica=new Logica();
+		miCoordinador= new Coordinador();
+		
+		/*Se establecen las relaciones entre clases*/
+		miVentanaPrincipal.setCoordinador(miCoordinador);
+		miVentanaRegistro.setCoordinador(miCoordinador);
+		miVentanaBuscar.setCoordinador(miCoordinador);
+		miLogica.setCoordinador(miCoordinador);
+		
+		/*Se establecen relaciones con la clase coordinador*/
+		miCoordinador.setMiVentanaPrincipal(miVentanaPrincipal);
+		miCoordinador.setMiVentanaRegistro(miVentanaRegistro);
+		miCoordinador.setMiVentanaBuscar(miVentanaBuscar);
+		miCoordinador.setMiLogica(miLogica);
+				
+		miVentanaPrincipal.setVisible(true);
+    }
+
+}
