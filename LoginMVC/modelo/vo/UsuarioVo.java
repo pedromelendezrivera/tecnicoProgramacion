c7d6b66fff2fb5b37b20fa9316a8e19c160cb368
Pedro Melendez <pmelendezrivera@mail.unicundi.edu.co>
2018-11-15 11:13:23 -0500

    Create UsuarioVo.java

---
+++ LoginMVC/modelo/vo/UsuarioVo.java
@@ -0,0 +1,84 @@
+package modelo.vo;
+
+/**
+ *
+ * @author PMELENDEZ
+ */
+public  class UsuarioVo {
+
+    
+    private int id;
+    private String usuario;
+    private String password;
+    private String nombre;
+    private String email;
+    private String last_session;
+    private int idTipo;
+
+    public UsuarioVo() {
+        
+    }
+
+    
+    public UsuarioVo(String usuario, String password) {
+          this.usuario = usuario;
+          this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLast_session() {
+        return last_session;
+    }
+
+    public void setLast_session(String last_session) {
+        this.last_session = last_session;
+    }
+
+    public int getIdTipo() {
+        return idTipo;
+    }
+
+    public void setIdTipo(int idTipo) {
+        this.idTipo = idTipo;
+    }
+    
+}
